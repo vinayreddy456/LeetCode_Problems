e5e65d81851bb2b68c56a4768339ea88fdea5bcf
ANNAPUREDDY VINAY KUMAR REDDY <20121A0509@vidyanikethan.asia>
2023-08-09 09:44:15 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (83.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    int solve(int n,int[] dp){
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+       
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    }
+    public int climbStairs(int n) {
+      int[] dp=new int[n+1];
+      Arrays.fill(dp,-1);
+      int x=solve(n,dp);  
+      return x;
+    }
+}
