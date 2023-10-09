d6c9dc91d9d337cd47229df98caea8fa99b2374c
ANNAPUREDDY VINAY KUMAR REDDY <20121A0509@vidyanikethan.asia>
2023-10-09 17:59:21 +0530

    Added solution - LeetHub

---
+++ Topological sort - GFG/topological-sort.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            String st[] = read.readLine().trim().split("\\s+");
+            int edg = Integer.parseInt(st[0]);
+            int nov = Integer.parseInt(st[1]);
+
+            for (int i = 0; i < nov; i++)
+                list.add(i, new ArrayList<Integer>());
+
+            int p = 0;
+            for (int i = 1; i <= edg; i++) {
+                String s[] = read.readLine().trim().split("\\s+");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                list.get(u).add(v);
+            }
+
+            int[] res = new Solution().topoSort(nov, list);
+
+            if (check(list, nov, res) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+    static boolean check(ArrayList<ArrayList<Integer>> list, int V, int[] res) {
+        
+        if(V!=res.length)
+        return false;
+        
+        int[] map = new int[V];
+        for (int i = 0; i < V; i++) {
+            map[res[i]] = i;
+        }
+        for (int i = 0; i < V; i++) {
+            for (int v : list.get(i)) {
+                if (map[i] > map[v]) return false;
+            }
+        }
+        return true;
+    }
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static void dfs(int source,Stack<Integer> stack,boolean[] vis,ArrayList<ArrayList<Integer>> adj){
+        vis[source]=true;
+        for(Integer i:adj.get(source)){
+            if(!vis[i]){
+                dfs(i,stack,vis,adj);
+            }
+        }
+        stack.push(source);
+    }
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+        int[] res=new int[V];
+        boolean[] visited=new boolean[V];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                dfs(i,stack,visited,adj);
+            }
+        }
+        int k=0;
+        while(!stack.isEmpty()){
+            res[k++]=stack.pop();
+        }
+        return res;
+    }
+}
